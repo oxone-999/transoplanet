ef755853b81a817df9d570b8fd91ad49c03d3a32
Anuj6545 <vikalpraj07@gmail.com>
2023-10-23 03:16:48 +0530

    add

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from '../../styles/Home.module.css'
+
+function Home() {
+  return (
+    <div className={Styles.main}>
+        <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
